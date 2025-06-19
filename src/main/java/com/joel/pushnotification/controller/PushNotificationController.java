2259aaab851ca6c431d40eb7a5b4041733ab6095
Joel Oliveira <joel80851@gmail.com>
2025-06-19 12:03:25 +0530

    implemented working model of fcm

---
+++ src/main/java/com/joel/pushnotification/controller/PushNotificationController.java
@@ -0,0 +1,29 @@
+package com.joel.pushnotification.controller;
+
+import com.joel.pushnotification.model.PushNotificationRequest;
+import com.joel.pushnotification.model.PushNotificationResponse;
+import com.joel.pushnotification.service.PushNotificationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PushNotificationController {
+	
+	
+    private PushNotificationService pushNotificationService;
+    
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+    
+    @PostMapping("/notification/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        System.out.println("princr");
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+    
+}
